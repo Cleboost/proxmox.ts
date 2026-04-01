a701e05e126b437b3c6d19e5746aa80b5fdd3eee
Cleboost <clement.balarot@gmail.com>
2026-04-01 13:45:08 +0200

    build: update version to 0.3.0 and add prepack script

--- rc.sh
+++
@@ -1,5 +0,0 @@
-BASE_VERSION=$(bun -p "require('./package.json').version")
-COMMIT_HASH=$(git rev-parse --short HEAD)
-NEW_VERSION="$BASE_VERSION-rc.$COMMIT_HASH"
-npm version $NEW_VERSION --no-git-tag-version
-bun publish --tag rc
